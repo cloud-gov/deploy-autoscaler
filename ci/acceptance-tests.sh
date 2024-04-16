76c3a47b68f45746fd066241a3c0f9e016518cec
Christopher Weibel <christopher.weibel@gsa.gov>
2024-04-16 10:55:03 -0400

    Pulling golang and cli versions

--- ci/acceptance-tests.sh
+++ ci/acceptance-tests.sh
@@ -5,3 +5,7 @@ set -eu
 #CF_API_URL=$(echo "${CF_API_URL}" | sed 's/\/$//')
 
 echo "GOLANG_VERSION passed in is: ${GOLANG_VERSION}"
+
+echo "This is the installed go version: $(go version)"
+
+echo "This is the installed cf cli version: $(cf -v)"
