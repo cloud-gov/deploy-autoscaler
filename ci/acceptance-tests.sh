a6fdcb245bf691a7c70224afe20d3a7f4e88ca3c
Christopher Weibel <christopher.weibel@gsa.gov>
2024-04-29 14:25:25 -0400

    Attempt to parallelize some of the tests

--- ci/acceptance-tests.sh
+++ ci/acceptance-tests.sh
@@ -50,7 +50,7 @@ export GINKGO_BINARY=$PWD/ginkgo_v2_linux_amd64
 echo "###################################"
 echo "Running ${COMPONENT_TO_TEST} test..."
 echo "###################################"
-./bin/test ${COMPONENT_TO_TEST}
+./bin/test ${COMPONENT_TO_TEST} --nodes=4 #--flake-attempts=3
 
 
 
